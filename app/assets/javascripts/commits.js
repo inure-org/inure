e249dccd36ae871f67dc78e76805737a590caff1
grivo <90540827+grvo@users.noreply.github.com>
2023-08-21 09:47:57 -0300

    adição de script para atribuições dos commits

---
+++ app/assets/javascripts/commits.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $(".day-commits-table li.commit").live('click', function (e) {
+        if (e.target.nodeName != "A") {
+            location.href = $(this).attr("url");
+
+            e.stopPropagation();
+
+            return false;
+        }
+    });
+});
