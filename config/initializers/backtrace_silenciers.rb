a0a7536c21a41992f82f7fa2e695fa64575984fa
grivo <90540827+grvo@users.noreply.github.com>
2023-09-29 08:14:42 -0300

    silenciadores de backtrace para bibliotecas

---
+++ config/initializers/backtrace_silenciers.rb
@@ -0,0 +1,7 @@
+# certificar de reiniciar o servidor quando esse arquivo for modificado
+
+# você pode adicionar silenciadores backtrace para bibliotecas que estão sendo utilizadas mas você não quer ver nos seus backtraces
+# Rails.backtrace_cleaner.add_listener { |line| line =~ /my_noisy_library/ }
+
+# você pode também remover todos os silenciadores caso esteja tentando debugar um problema
+# Rails.backtrace_cleaner.remove_silencers!
