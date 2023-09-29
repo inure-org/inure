c8bbe6cdbee602a89214e8589de44a6d909dbe8b
grivo <90540827+grvo@users.noreply.github.com>
2023-09-29 08:15:58 -0300

    inicializador de regras inflection

---
+++ config/initializers/inflections.rb
@@ -0,0 +1,11 @@
+# certificar de reiniciar o servidor quando esse arquivo for modificado
+
+# adicionar novas regras de inflection utilizando o seguinte formato
+# (todos esses exemplos são ativos por padrão):
+#
+# ActiveSupport::Inflector.inflections do |inflect|
+#     inflect.plural /^(ox)$/i, '\1en'
+#     inflect.singular /^(ox)en/i, '\1'
+#     inflect.irregular 'person', 'people'
+#     inflect.uncountable %w( fish sheep )
+# end
